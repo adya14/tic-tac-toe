19be8a5cfa5a5b6fc72c75d90bcbca45ca880e93
TanyaPat01 <122528737+TanyaPat01@users.noreply.github.com>
2023-02-26 14:39:12 +0530

    Modify the game

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ symbol_thickness = 50
 symbol_X_color = '#EE4035'
 symbol_O_color = '#0492CF'
 Green_color = '#7BC043'
-
+#hsdfasjdgvfavdhdgvjaxgysuvjaxsjvnahbshjsvbsg
 
 class Tic_Tac_Toe():
     # ------------------------------------------------------------------
